5c0a8f71f086dcb55f6adf77190651ff3d5dcd5e
UMARSIDDD <umarsidd098@gmail.com>
2023-03-03 12:13:17 +0530

    splasjscreen design

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,4 +1,4 @@
-
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -8,13 +8,11 @@ import 'home.dart';
 import 'navbar.dart';
 
 class splashScreen extends StatefulWidget {
-
   @override
   State<splashScreen> createState() => _splashScreenState();
 }
 
 class _splashScreenState extends State<splashScreen> {
-
   @override
   void initState() {
     // TODO: implement initState
@@ -22,23 +20,45 @@ class _splashScreenState extends State<splashScreen> {
     _navigatetohome();
   }
 
-  _navigatetohome()async{
-    await Future.delayed(Duration(seconds: 3),(){});
+  _navigatetohome() async {
+    await Future.delayed(Duration(seconds: 3), () {});
     Navigator.pushReplacement(
-      this.context, MaterialPageRoute(builder: (context)=>navBar()) );
+        this.context, MaterialPageRoute(builder: (context) => navBar()));
   }
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Container(
-        child: Center(
-          child: Text(
-            'Farmer Connect',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xffbcfb69), Color(0xff26bbac)],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
           ),
         ),
-        
+        // color: Colors.green.shade300,
+        child: Center(
+          child: Container(
+            height: height * 0.2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/farmer.webp"),
+                  radius: 50,
+                ),
+                Text(
+                  'Farmer',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
